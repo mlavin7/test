f30263e33e127b9c02c9b2834ee77d2a1af51c6f
mlavin7 <mlavin7@gmail.com>
2023-12-20 11:13:08 +0100

    final

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let comment= document.getElementById("comment")
 
 
 function happy(){
-    emoji.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="600" height="600" fill="green" class="bi bi-emoji-heart-eyes" viewBox="0 0 16 16">
+    emoji.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" fill="green" class="bi bi-emoji-heart-eyes" viewBox="0 0 16 16">
 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
 <path d="M11.315 10.014a.5.5 0 0 1 .548.736A4.498 4.498 0 0 1 7.965 13a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .548-.736h.005l.017.005.067.015.252.055c.215.046.515.108.857.169.693.124 1.522.242 2.152.242.63 0 1.46-.118 2.152-.242a26.58 26.58 0 0 0 1.109-.224l.067-.015.017-.004.005-.002zM4.756 4.566c.763-1.424 4.02-.12.952 3.434-4.496-1.596-2.35-4.298-.952-3.434m6.488 0c1.398-.864 3.544 1.838-.952 3.434-3.067-3.554.19-4.858.952-3.434"/>
 </svg>`
@@ -16,7 +16,7 @@ function happy(){
 }
 
 function sad(){
-    emoji.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="600" height="600" fill="red" class="bi bi-emoji-dizzy" viewBox="0 0 16 16">
+    emoji.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" fill="red" class="bi bi-emoji-dizzy" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
   <path d="M9.146 5.146a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708m-5 0a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 1 1 .708.708l-.647.646.647.646a.5.5 0 1 1-.708.708L5.5 7.207l-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zM10 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0"/>
 </svg> `
